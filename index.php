dd23ba0302466343bc24338d514c4584d09ba430
aburayhan12 <136449209+aburayhan12@users.noreply.github.com>
2023-09-19 22:14:35 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<h2> num1= 40</h3>";
+echo "<h2> num2= 30</h3>";
+
+$num1 = 40;
+$num2 = 30;
+
+echo $sum = "<p> Sum of num1 and num2 is: </p>" . $num1 + $num2;
+echo $sum = "<p> Substraction of num1 and num2 is: </p>" . $num1 - $num2;
+echo $sum = "<p> Multiplication of num1 and num2 is: </p>" . $num1 * $num2;
+echo $sum = "<p> Division of num1 and num2 is: </p>" . $num1 / $num2;
+echo $sum = "<p> Modulas of num1 and num2 is: </p>" . $num1 % $num2;
+
+
+$first_name = 'Abu ';
+$last_name = 'Rayhan';
+echo $full_name = '<h4>Full name is: </h4>' . $first_name . $last_name; 
+
+
+
+?>
